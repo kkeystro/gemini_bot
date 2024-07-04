4ea2a8442ef47a795f4584017b45c1aa43181e6a
kkeystro <79106189+kkeystro@users.noreply.github.com>
2024-07-04 18:57:52 +0300

    minor changes

--- mdfuck.py
+++ mdfuck.py
@@ -1,45 +1,24 @@
-import re
-
 import markdown2
 from bs4 import BeautifulSoup
 
 
 def clean_html(html_text):
-    # Список поддерживаемых HTML тегов в aiogram
     supported_tags = ['b', 'strong', 'i', 'em', 'u', 's', 'strike', 'del', 'ins', 'a', 'code', 'pre']
 
-    # Сначала защитим выражения вроде "if a <b and b>c"
-    protected_expressions = []
-
-    def protect_expression(match):
-        protected_expressions.append(match.group())
-        return f"PROTECTED_EXPRESSION_{len(protected_expressions) - 1}"
-
-    html_text = re.sub(r'\b\w+\s*[<>]\s*\w+(\s+(and|or)\s+\w+\s*[<>]\s*\w+)*', protect_expression, html_text)
-
-    # Парсим HTML
     soup = BeautifulSoup(html_text, 'html.parser')
 
-    # Удаляем неподдерживаемые теги, сохраняя их содержимое
     for tag in soup.find_all():
         if tag.name not in supported_tags:
             tag.unwrap()
 
-    # Преобразуем обратно в строку
     cleaned_html = str(soup)
-
-    # Восстанавливаем защищенные выражения
-    for i, expr in enumerate(protected_expressions):
-        cleaned_html = cleaned_html.replace(f"PROTECTED_EXPRESSION_{i}", expr)
 
     return cleaned_html
 
 
 def escape_markdown_v2(md_text):
-    # Convert Markdown to HTML
     html = markdown2.markdown(md_text, extras=["markdown-in-html"])
 
-    # Clean HTML by removing unsupported tags
     cleaned_html = clean_html(html)
 
     return cleaned_html
--- routers/starthandler.py
+++ routers/starthandler.py
@@ -12,20 +12,11 @@ router = Router()
 
 
 @router.message(Command("start"))
-async def cmd_start(message: Message, state: FSMContext):
+async def add_key_1(message: Message, state: FSMContext):
     await storage.delkey(key=str(message.from_user.id))
     await storage.unlock(str(message.from_user.id))
     await message.answer(
-        text="Добавьте ключ API",
-    )
-    await state.set_state(UserStates.choosing_not_to_pay)
-
-
-@router.message(UserStates.choosing_not_to_pay)
-async def add_key_1(message: Message, state: FSMContext):
-    await state.update_data(chosen_way=message.text.lower())
-    await message.answer(
-        "Отправьте ключ, получить ключ можно по ссылке:https://aistudio.google.com/app/prompts/new_chat",
+        'Отправьте ключ, <a href="https://aistudio.google.com/app/apikey"> получить ключ</a>',
     )
     await state.set_state(UserStates.adding_key)
 
@@ -35,7 +26,7 @@ async def add_key_2(message: Message, state=FSMContext):
     if await check_key(message.text):
         await add_api_key(message.text)
         await message.answer(
-            "Ключ добавлен",
+            "Ключ добавлен, добро пожаловать в мир генеративного ИИ!",
             await state.set_state(UserStates.user_not_premium)
         )
     else:
